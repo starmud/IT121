a57d4ebd23960bed45132b572cfa883fe92cb736
starmud <45653563+starmud@users.noreply.github.com>
2025-02-03 03:16:22 -0800

    Update script.js
    updated to assignment outline

--- week1/script.js
+++ week1/script.js
@@ -4,4 +4,3 @@ let firstName = "David";
 let lastName = "Charlebois";
 let yearsOfStudy = "2.5";
 let goal = "Web development & design";
-document.write("My name is " + firstName + " " + lastName + ", I've attended NSC & SCC for" + yearsOfStudy + " years. I plan to pursue " + goal + " after completing my studies.")
